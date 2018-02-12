e106e7abebd57a690cb115dcd26f366fba638614
GSCHOF11 <gregs@indellient.com>
2018-02-11 19:34:09 -0500

    add text area, logo and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="./assets/toml-200.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        toml lint
+      </a>
+    </nav>
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="code" class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">With textarea</span>
+      </div>
+      <textarea class="form-control" aria-label="With textarea" rows="30"></textarea>
+    </div>
+    <div id="validate">
+      <button type="button" class="btn btn-primary btn-lg">Validate</button>
+      <button type="button" class="btn btn-secondary btn-lg">Clear</button>
+    </div>
   </div>
 </template>
 
@@ -24,7 +25,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Paste toml file below to lint'
     }
   }
 }
@@ -37,7 +38,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 
 h1, h2 {
@@ -56,5 +57,9 @@ li {
 
 a {
   color: #42b983;
+}
+
+#validate {
+  margin-top: 10px
 }
 </style>
